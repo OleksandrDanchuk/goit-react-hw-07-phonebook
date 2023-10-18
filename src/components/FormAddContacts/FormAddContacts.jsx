f3175adf7b5b7b026339e005699a8805406287a1
OleksandrDanchuk <117369013+OleksandrDanchuk@users.noreply.github.com>
2023-10-18 16:04:54 +0300

    fix

--- src/components/FormAddContacts/FormAddContacts.jsx
+++ src/components/FormAddContacts/FormAddContacts.jsx
@@ -1,27 +1,49 @@
 import { useState } from 'react';
 import css from './FormAddContacts.module.css';
+import { nanoid } from '@reduxjs/toolkit';
 import { useDispatch, useSelector } from 'react-redux';
 import { addContact } from 'components/redux/operations';
 import { getContacts } from 'components/redux/selectors';
 
 export const FormAddContacts = () => {
-  const [contact, setContact] = useState({ name: '', number: '' });
-  const contacts = useSelector(getContacts);
   const dispatch = useDispatch();
+  const currentContacts = useSelector(getContacts);
+
+  const [state, setState] = useState({
+    name: '',
+    number: '',
+  });
+
+  const handleChange = ({ target: { name, value } }) => {
+    setState(prevState => ({ ...prevState, [name]: value }));
+  };
 
   const submitAddContact = evt => {
     evt.preventDefault();
-    const isExist = contacts.find(
-      elem => elem.name.toLowerCase() === contact.name.toLowerCase()
-    );
-    if (isExist) return alert(`${contact.name} is already in contacts`);
-    dispatch(addContact(contact));
-    setContact({ name: '', number: '' });
-  };
+    const { name, number } = evt.target.elements;
+    if (!state.name.trim() || !state.number.trim()) {
+      alert('Please enter the correct values');
+      return;
+    }
+    const newContact = {
+      id: nanoid(),
+      name: state.name,
+      number: state.number,
+    };
 
-  const handleOnChange = event => {
-    const { name, value } = event.target;
-    setContact({ ...contact, [name]: value });
+    const сheckRepetition = currentContacts.find(
+      ({ contact }) =>
+        contact.name.toLowerCase() === name.value.toLowerCase() ||
+        contact.number === number.value
+    );
+
+    сheckRepetition
+      ? alert(
+          `${name.value} or  number: ${number.value} is already in contacts.`
+        )
+      : dispatch(addContact(newContact));
+
+    setState({ name: '', number: '' });
   };
 
   return (
@@ -30,27 +52,27 @@ export const FormAddContacts = () => {
         <label className={css.label}>
           Name
           <input
-            value={contact.name}
-            onChange={handleOnChange}
             className={css.input}
             type="text"
             name="name"
             pattern="^[a-zA-Z\s]+$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
+            onChange={handleChange}
+            value={state.name}
           />
         </label>
         <label className={css.label}>
           Number
           <input
-            value={contact.number}
-            onChange={handleOnChange}
             className={css.input}
             type="tel"
             name="number"
             pattern="^[0-9]+$"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
+            onChange={handleChange}
+            value={state.number}
           />
         </label>
         <button className={css.form_button}>Add contact</button>
